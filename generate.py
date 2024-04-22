bd2948e02555586fb9a03c4c103150410334c918
Alexej Doronin <lyosha.doronin@gmail.com>
2024-04-22 22:33:50 +0200

    generator fixes

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
-from dataclasses import dataclass
 from pathlib import Path
 import re
+import struct
 
 valid_types = {
     "int8": ("b", "int"),
@@ -19,7 +19,16 @@ valid_types = {
 ident = re.compile(r'^[A-Za-z_][A-Za-z0-9_]*$')
 nl = "\n"
 
-c_templ = """#pragma once
+class CTempl:
+    Parse = """
+    memcpy(&out->{name}, src, sizeof(out->{name}));
+    src += sizeof(out->{name});"""
+
+    Dump = """
+    memcpy(buff, &obj->{name}, sizeof(obj->{name}));
+    buff += sizeof(obj->{name});"""
+
+    Main = """#pragma once
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
@@ -30,42 +39,59 @@ typedef double float64_t;
 struct {name} {{
 {fields}}};
 
-void parse_{name}({name}* out, const void* __restrict__ src) {{
-    memcpy(out, src, sizeof({name}));
+static inline size_t parse_{name}({name}* out, const char* __restrict__ src, size_t size) {{
+    if (size < {total_size}) return 0;{parse_fields}
+    return {total_size};
+}}
+static inline size_t dump_{name}({name}* obj, char* __restrict__ buff, size_t size) {{
+    if (size < {total_size}) return 0;{dump_fields}
+    return {total_size};
 }}
 """
+    @staticmethod
+    def format(name, fields, total_size):
+        return CTempl.Main.format_map({
+            "name": name,
+            "fields": f"{''.join(f'    {t}_t {n};{nl}' for n, t in fields.items())}",
+            "total_size": total_size,
+            "parse_fields": ''.join(CTempl.Parse.format_map({
+                "name": n
+            }) for n, t in fields.items()),
+            "dump_fields": ''.join(CTempl.Dump.format_map({
+                "name": n
+            }) for n, t in fields.items())
+        })
 
-def format_c(name, fields):
-    return c_templ.format_map({
-        "name": name,
-        "fields": f"{''.join([f'    {t}_t {n};{nl}' for t, n in fields.items()])}"
-    })
-
-py_templ = """import struct
+class PyTempl:
+    Main = """import struct
 import sys
-from dataclasses import dataclass
-
-assert sys.byteorder == "little"
-
-raw_{name} = struct.Struct("{fmt}")
+from dataclasses import dataclass, fields
 
 @dataclass
 class {name}:
 {names}
     @staticmethod
     def from_buffer(buff):
-        return {name}(*raw_{name}.unpack(buff))
+        return {name}(*{name}._s.unpack(buff))
+    def into_buffer(self):
+        return {name}._s.pack(getattr(self, n) for n in {name}._names)
 
+{name}._names = tuple(f.name for f in fields({name}))
+{name}._s = struct.Struct("{fmt}")
+        
 """
-
-def format_py(name, fields):
-    fmt = "@" + "".join([valid_types[t][0] for t in fields])
-    names = "".join([f'    {n}: {valid_types[t][1]}{nl}' for t, n in fields.items()])
-    return py_templ.format_map({
-        "name": name, 
-        "names": names, 
-        "fmt": fmt
-    })
+    @staticmethod
+    def struct_fmt(fields):
+        return "<" + "".join([valid_types[t][0] for n, t in fields.items()])
+    @staticmethod
+    def format(name, fields):
+        fmt = PyTempl.struct_fmt(fields)
+        names = "".join([f'    {n}: {valid_types[t][1]}{nl}' for n, t in fields.items()])
+        return PyTempl.Main.format_map({
+            "name": name, 
+            "names": names, 
+            "fmt": fmt
+        })
 
 def main():
     parser = ArgumentParser()
@@ -85,16 +111,20 @@ def main():
             t, name = line.split()
             if not ident.match(name):
                 raise RuntimeError(f"Invalid identifier: {name}")
-            fields[t] = name
-    c_result = format_c(src.stem, fields)
-    py_result = format_py(src.stem, fields)
+            fields[name] = t
+    c_result = CTempl.format(src.stem, fields, struct.calcsize(PyTempl.struct_fmt(fields)))
+    py_result = PyTempl.format(src.stem, fields)
     if args.stdout:
-        print(f"Python: {py_result}")
-        print(f"C Header: {c_result}")
+        print(f"Python: ---\n\n{py_result}\n\n")
+        print(f"C Header: ---\n\n{c_result}\n\n")
+    def do_open(ext):
+        p = Path(args.out + ext)
+        p.parent.mkdir(parents=True, exist_ok=True)
+        return open(p, "+w")
     if args.out:
-        with open(args.out + ".h", "+w") as f:
+        with do_open(".h") as f:
             f.write(c_result)
-        with open(args.out + ".py", "+w") as f:
+        with do_open(".py") as f:
             f.write(py_result)
 
 
