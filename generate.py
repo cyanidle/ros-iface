3651a276306341f76fb9417975f94ed71ae889e5
Alexej Doronin <lyosha.doronin@gmail.com>
2024-04-23 23:46:17 +0200

    renameable codegen

--- generate.py
+++ generate.py
@@ -99,6 +99,7 @@ def main():
     parser.add_argument("--stdout", "-s", default=False, action="store_true", help="print to stdout")
     parser.add_argument("--out_py", type=str, default="", help="python output file")
     parser.add_argument("--out_c", type=str, default="", help="c output file")
+    parser.add_argument("--name", type=str, default="", help="class name")
     args, _ = parser.parse_known_args()
     fields = []
     src = Path(args.file)
@@ -110,8 +111,9 @@ def main():
             if not ident.match(name):
                 raise RuntimeError(f"Invalid identifier: {name}")
             fields.append((name, t))
-    c_result = CTempl.format(src.stem, fields, struct.calcsize(PyTempl.struct_fmt(fields)))
-    py_result = PyTempl.format(src.stem, fields)
+    name = args.name or src.stem
+    c_result = CTempl.format(name, fields, struct.calcsize(PyTempl.struct_fmt(fields)))
+    py_result = PyTempl.format(name, fields)
     if args.stdout:
         print(f"Python: ---\n\n{py_result}\n\n")
         print(f"C Header: ---\n\n{c_result}\n\n")
