33bd94573800806471b0c0660bbac457b327ed0f
Alexej Doronin <lyosha.doronin@gmail.com>
2024-04-24 23:37:25 +0200

    support constants

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
 import re
 import struct
+from typing import Iterable, List, Optional
 
 valid_types = {
     "int8": ("b", "int"),
@@ -16,7 +18,12 @@ valid_types = {
     "float64": ("d", "float"),
 }
 
-ident = re.compile(r'^[A-Za-z_][A-Za-z0-9_]*$')
+@dataclass
+class Field:
+    type: str
+    name: str
+    const: Optional[str]
+
 nl = "\n"
 
 class CTempl:
@@ -36,6 +43,7 @@ class CTempl:
 typedef float float32_t;
 typedef double float64_t;
 
+{consts}
 struct {name} {{
 {fields}}};
 
@@ -43,29 +51,33 @@ static inline size_t parse_{name}({name}* out, const char* __restrict__ src, siz
     if (size < {total_size}) return 0;{parse_fields}
     return {total_size};
 }}
+
 static inline size_t dump_{name}({name}* obj, char* __restrict__ buff, size_t size) {{
     if (size < {total_size}) return 0;{dump_fields}
     return {total_size};
 }}
 """
     @staticmethod
-    def format(name, fields, total_size):
+    def format_consts(name, fields: Iterable[Field]):
+        items = ''.join(f"    {f.name} = {f.const},{nl}" for f in fields if f.const)
+        if items: return """typedef enum {{\n{}}} {}_;\n""".format(items, name)
+        else: return ""
+    @staticmethod
+    def format(name, fields: Iterable[Field], total_size):
         return CTempl.Main.format_map({
             "name": name,
-            "fields": f"{''.join(f'    {t}_t {n};{nl}' for n, t in fields)}",
+            "fields": f"{''.join(f'    {f.type}_t {f.name};{nl}' for f in fields if not f.const)}",
             "total_size": total_size,
-            "parse_fields": ''.join(CTempl.Parse.format_map({
-                "name": n
-            }) for n, t in fields),
-            "dump_fields": ''.join(CTempl.Dump.format_map({
-                "name": n
-            }) for n, t in fields)
+            "parse_fields": ''.join(CTempl.Parse.format_map({"name": f.name}) for f in fields if not f.const),
+            "dump_fields": ''.join(CTempl.Dump.format_map({"name": f.name}) for f in fields if not f.const),
+            "consts": CTempl.format_consts(name, fields)
         })
 
 class PyTempl:
     Main = """import struct
 import sys
 from dataclasses import dataclass, fields
+from typing import ClassVar
 
 @dataclass
 class {name}:
@@ -81,17 +93,34 @@ class {name}:
         
 """
     @staticmethod
-    def struct_fmt(fields):
-        return "<" + "".join([valid_types[t][0] for n, t in fields])
+    def remap_fmt(t: str): 
+        return valid_types[t][0]
     @staticmethod
-    def format(name, fields):
+    def remap_type(t: str): 
+        return valid_types[t][1]
+    @staticmethod
+    def struct_fmt(fields: Iterable[Field]):
+        return "<" + "".join(PyTempl.remap_fmt(f.type) for f in fields if not f.const)
+    @staticmethod
+    def format(name, fields: Iterable[Field]):
         fmt = PyTempl.struct_fmt(fields)
-        names = "".join([f'    {n}: {valid_types[t][1]}{nl}' for n, t in fields])
+        names = "".join(f'    {f.name}: {PyTempl.remap_type(f.type)}{nl}' for f in fields if not f.const)
+        names += "".join(f'    {f.name}: ClassVar[{PyTempl.remap_type(f.type)}] = {f.const}{nl}' for f in fields if f.const)
         return PyTempl.Main.format_map({
             "name": name, 
             "names": names, 
             "fmt": fmt
         })
+
+def parse_line(line: str) -> Optional[Field]:
+    parts = line.split()
+    if len(parts) == 2:
+        return Field(parts[0], parts[1], None)
+    elif len(parts) == 4:
+        if not parts[2] == "=": raise RuntimeError(f"Line should be [<type> <name> = <const>], was: {line}")
+        return Field(parts[0], parts[1], parts[3])
+    else:
+        return None
 
 def main():
     parser = ArgumentParser()
@@ -101,16 +130,15 @@ def main():
     parser.add_argument("--out_c", type=str, default="", help="c output file")
     parser.add_argument("--name", type=str, default="", help="class name")
     args, _ = parser.parse_known_args()
-    fields = []
+    fields: List[Field] = []
     src = Path(args.file)
-    if not ident.match(src.stem):
+    if not re.match(r'([A-Za-z_][A-Za-z0-9_]*)', src.stem):
         raise RuntimeError(f".msg file name is not allowed: {src.name}")
     with open(src, "r") as f:
         for line in f.readlines():
-            t, name = line.split()
-            if not ident.match(name):
-                raise RuntimeError(f"Invalid identifier: {name}")
-            fields.append((name, t))
+            f = parse_line(line)
+            if not f is None: 
+                fields.append(f)
     name = args.name or src.stem
     c_result = CTempl.format(name, fields, struct.calcsize(PyTempl.struct_fmt(fields)))
     py_result = PyTempl.format(name, fields)
